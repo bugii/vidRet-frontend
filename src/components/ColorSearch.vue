6d9ca3d29a05685f75401ee7bac0b1b60008bac9
Dario Bugmann <dario.bugmann@outlook.com>
2021-05-14 14:57:10 +0200

    changed LAB implementation using an 8x8 grid

--- src/components/ColorSearch.vue
+++ src/components/ColorSearch.vue
@@ -7,14 +7,38 @@
     <input v-model="selectedColor" type="color" id="color" name="color" />
 
     <div class="canvas-wrapper">
-      <canvas
+      <!-- <canvas
         @mousedown="startPainting"
         @mouseup="finishedPainting"
         @mousemove="draw"
         width="512"
         height="512"
         ref="canvas"
-      ></canvas>
+      ></canvas> -->
+      <!-- <div class="row" v-for="y in colorsRGB.length" :key="y">
+        <div
+          v-for="x in y.length"
+          :key="x"
+          :style="{
+            backgroundColor: `rgb(${colorsRGB[y][x][0]},${colorsRGB[y][x][1]},${colorsRGB[y][x][2]}`,
+          }"
+          @click="setColor(y, x)"
+        >
+          {{ y }}-{{ x }}
+        </div>
+      </div> -->
+      <div class="row" v-for="y in colorsRGB.length" :key="y">
+        <div
+          v-for="x in colorsRGB[y - 1].length"
+          :key="x"
+          @click="setColor(y - 1, x - 1)"
+          :style="{
+            backgroundColor: `rgb(${colorsRGB[y - 1][x - 1][0]},${
+              colorsRGB[y - 1][x - 1][1]
+            },${colorsRGB[y - 1][x - 1][2]})`,
+          }"
+        ></div>
+      </div>
     </div>
 
     <button @click.prevent="search">
@@ -29,52 +53,185 @@ export default {
     return {
       selectedColor: "#00000",
       painting: false,
+      colorsRGB: [
+        [
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+        ],
+        [
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+        ],
+        [
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+        ],
+        [
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+        ],
+        [
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+        ],
+        [
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+        ],
+        [
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+        ],
+        [
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+          [255, 255, 255],
+        ],
+      ],
     };
+  },
+
+  computed: {
+    colorsLAB() {
+      return this.colorsRGB.map((r) => r.map((c) => this.rgb2lab(c)));
+    },
   },
   methods: {
     search() {
       console.log(
         "searching for class-labelled image",
-        this.$refs.canvas
-          .getContext("2d")
-          .getImageData(0, 0, this.$refs.canvas.width, this.$refs.canvas.height)
+        //   this.$refs.canvas
+        //     .getContext("2d")
+        //     .getImageData(0, 0, this.$refs.canvas.width, this.$refs.canvas.height)
+
+        this.colorsLAB
       );
     },
 
-    getMousePos(evt) {
-      var rect = this.$refs.canvas.getBoundingClientRect(), // abs. size of element
-        scaleX = this.$refs.canvas.width / rect.width, // relationship bitmap vs. element for X
-        scaleY = this.$refs.canvas.height / rect.height; // relationship bitmap vs. element for Y
+    convertHex(hexCode) {
+      var hex = hexCode.replace("#", "");
 
-      return {
-        x: (evt.clientX - rect.left) * scaleX, // scale mouse coordinates after they have
-        y: (evt.clientY - rect.top) * scaleY, // been adjusted to be relative to element
-      };
+      if (hex.length === 3) {
+        hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
+      }
+
+      var r = parseInt(hex.substring(0, 2), 16),
+        g = parseInt(hex.substring(2, 4), 16),
+        b = parseInt(hex.substring(4, 6), 16);
+
+      return [r, g, b];
     },
-    startPainting(e) {
-      this.painting = true;
-      this.draw(e);
+
+    rgb2lab(rgb) {
+      var r = rgb[0] / 255,
+        g = rgb[1] / 255,
+        b = rgb[2] / 255,
+        x,
+        y,
+        z;
+
+      r = r > 0.04045 ? Math.pow((r + 0.055) / 1.055, 2.4) : r / 12.92;
+      g = g > 0.04045 ? Math.pow((g + 0.055) / 1.055, 2.4) : g / 12.92;
+      b = b > 0.04045 ? Math.pow((b + 0.055) / 1.055, 2.4) : b / 12.92;
+
+      x = (r * 0.4124 + g * 0.3576 + b * 0.1805) / 0.95047;
+      y = (r * 0.2126 + g * 0.7152 + b * 0.0722) / 1.0;
+      z = (r * 0.0193 + g * 0.1192 + b * 0.9505) / 1.08883;
+
+      x = x > 0.008856 ? Math.pow(x, 1 / 3) : 7.787 * x + 16 / 116;
+      y = y > 0.008856 ? Math.pow(y, 1 / 3) : 7.787 * y + 16 / 116;
+      z = z > 0.008856 ? Math.pow(z, 1 / 3) : 7.787 * z + 16 / 116;
+
+      return [116 * y - 16, 500 * (x - y), 200 * (y - z)];
     },
-    finishedPainting() {
-      this.painting = false;
-      this.$refs.canvas.getContext("2d").beginPath();
+
+    setColor(y, x) {
+      const rgbColor = this.convertHex(this.selectedColor);
+      console.log("setting color for", y, x, rgbColor);
+      this.$set(this.colorsRGB[y], x, rgbColor);
     },
-    draw(e) {
-      if (!this.painting) return;
 
-      this.$refs.canvas.getContext("2d").lineWidth = 30;
-      // TODO: convert color to CIE Lab
-      this.$refs.canvas.getContext("2d").strokeStyle = this.selectedColor;
-      this.$refs.canvas.getContext("2d").lineCap = "round";
+    // getMousePos(evt) {
+    //   var rect = this.$refs.canvas.getBoundingClientRect(), // abs. size of element
+    //     scaleX = this.$refs.canvas.width / rect.width, // relationship bitmap vs. element for X
+    //     scaleY = this.$refs.canvas.height / rect.height; // relationship bitmap vs. element for Y
 
-      const mousePos = this.getMousePos(e);
+    //   return {
+    //     x: (evt.clientX - rect.left) * scaleX, // scale mouse coordinates after they have
+    //     y: (evt.clientY - rect.top) * scaleY, // been adjusted to be relative to element
+    //   };
+    // },
+    // startPainting(e) {
+    //   this.painting = true;
+    //   this.draw(e);
+    // },
+    // finishedPainting() {
+    //   this.painting = false;
+    //   this.$refs.canvas.getContext("2d").beginPath();
+    // },
+    // draw(e) {
+    //   if (!this.painting) return;
 
-      this.$refs.canvas.getContext("2d").lineTo(mousePos.x, mousePos.y);
-      this.$refs.canvas.getContext("2d").stroke();
+    //   this.$refs.canvas.getContext("2d").lineWidth = 30;
+    //   // TODO: convert color to CIE Lab
+    //   this.$refs.canvas.getContext("2d").strokeStyle = this.selectedColor;
+    //   this.$refs.canvas.getContext("2d").lineCap = "round";
 
-      this.$refs.canvas.getContext("2d").beginPath();
-      this.$refs.canvas.getContext("2d").moveTo(mousePos.x, mousePos.y);
-    },
+    //   const mousePos = this.getMousePos(e);
+
+    //   this.$refs.canvas.getContext("2d").lineTo(mousePos.x, mousePos.y);
+    //   this.$refs.canvas.getContext("2d").stroke();
+
+    //   this.$refs.canvas.getContext("2d").beginPath();
+    //   this.$refs.canvas.getContext("2d").moveTo(mousePos.x, mousePos.y);
+    // },
   },
 };
 </script>
@@ -97,11 +254,22 @@ export default {
     min-height: 0;
     margin-top: 1rem;
 
-    canvas {
-      border: lightgray thin solid;
-      max-height: 100%;
-      max-width: 100%;
-      min-height: 0;
+    // canvas {
+    //   border: lightgray thin solid;
+    //   max-height: 100%;
+    //   max-width: 100%;
+    //   min-height: 0;
+    // }
+
+    .row {
+      width: 100%;
+      display: flex;
+
+      div {
+        width: 2rem;
+        height: 2rem;
+        border: thin gainsboro solid;
+      }
     }
   }
 }
